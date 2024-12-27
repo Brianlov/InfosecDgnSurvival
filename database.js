02f85b49ed223b6ab9ec0c5685bc0dd1dd0e50c7
Brian <brianooi1109@gmail.com>
2024-12-27 22:47:00 +0800

    new

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://b022210122:nogizakabrian@cluster0.wmjlp.mongodb.net/?retryWrites=true&w=majority&tls=true&appName=Cluster0";
+const uri = "mongodb+srv://b022210122:nogizakabrian@cluster0.wmjlp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -8,8 +8,7 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-  tls: true,
-  tlsAllowInvalidCertificates: false, // Set to true if using self-signed certificates
+  
 });
 
 module.exports = client
