e7252d177499e0df2ef42696c6c0db3e12fde052
Brian <brianooi1109@gmail.com>
2024-12-27 21:11:12 +0800

    new endpoint added

--- index.js
+++ index.js
@@ -55,6 +55,9 @@ app.use((req, res) => {
 })
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+app.get('/', (req, res) => {
+    res.send('Welcome to dungeon dive game!')
+});
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
 })
